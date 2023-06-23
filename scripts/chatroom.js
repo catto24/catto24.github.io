0ba7d256791d0f314fa97143ca49b3f3ff6bf699
Con <50535379+ConnorTippets@users.noreply.github.com>
2023-06-23 04:06:19 -0500

    center?
    yo?

--- scripts/chatroom.js
+++ scripts/chatroom.js
@@ -2,9 +2,14 @@ createButton = function(div, text, centerx, centery, width, height) {
   var button = document.createElement("button");
   button.textContent = text;
   
+  button.style.position = "absolute";
   if(centerx === true) {
-    button.style.display = "block";
-    button.style.margin = "auto";
+    button.style.left = "50%";
+    button.style.transform = "translateX(-50%)";
+  };
+  if(centerx === true) {
+    button.style.top = "50%";
+    button.style.transform = "translateY(-50%)";
   };
   button.style.width = width;
   button.style.height = height;
@@ -17,7 +22,8 @@ window.onload = function() {
   var div = document.getElementById("chatroom");
   div.style.width = "50%";
   div.style.height = "50%";
+  div.style.position = "relative";
   
-  var login = createButton(div, "Login", true, false, "50%", "50%");
+  var login = createButton(div, "Login", true, true, "50%", "10%");
   //var anon = createButton(div, "Continue as guest")
 };
