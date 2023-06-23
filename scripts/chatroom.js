16be62f51c051f395f330402abfdc60d78b384ae
Con <50535379+ConnorTippets@users.noreply.github.com>
2023-06-23 04:21:15 -0500

    add other button
    yay!!

--- scripts/chatroom.js
+++ scripts/chatroom.js
@@ -3,6 +3,7 @@ createButton = function(div, text, centerx, centery, width, height) {
   button.textContent = text;
   
   button.style.position = "absolute";
+  button.style.display = "block";
   button.style.transform = ""
   if(centerx === true) {
     button.style.left = "50%";
@@ -29,5 +30,5 @@ window.onload = function() {
   div.style.position = "relative";
   
   var login = createButton(div, "Login", true, true, "12.5%", "10%");
-  //var anon = createButton(div, "Continue as guest")
+  var anon = createButton(div, "Continue as guest", true, true, "50%", "10%");
 };
