d096de7ec574fc0f51cdf9cdcf29345970d22fa7
Con <50535379+ConnorTippets@users.noreply.github.com>
2023-06-23 04:39:18 -0500

    change some numbers around
    yknow a regular tuesday

--- scripts/chatroom.js
+++ scripts/chatroom.js
@@ -30,6 +30,6 @@ window.onload = function() {
   div.style.height = "50%";
   div.style.position = "relative";
   
-  var login = createButton(div, "Login", true, true, 0, 2.5);
-  var anon = createButton(div, "Continue as guest", true, true, 0, -2.5);
+  var login = createButton(div, "Login", true, true, 0, -2.75);
+  var anon = createButton(div, "Continue as guest", true, true, 0, 2.75);
 };
